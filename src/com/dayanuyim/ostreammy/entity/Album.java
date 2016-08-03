be90e871bf2677c495c8ffde1ed88e4770a1e5c6
TsungtaTsai <tsungtatsai@iii.org.tw>
2016-08-03 18:09:46 +0800

    update entities

--- src/com/dayanuyim/ostreammy/entity/Album.java
+++ src/com/dayanuyim/ostreammy/entity/Album.java
@@ -1,19 +1,26 @@
 package com.dayanuyim.ostreammy.entity;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.util.Objects;
 
 import javax.imageio.stream.ImageInputStream;
 
 public class Album {
+	private String id;
 	private String name;
-	private LocalDate publishDate;
-	private CompactDisk[] disks;
 	private Person artist;
-	private String[] booklet_paths;
+	private String genre;
+	private LocalDate publishDate;
+	private Corporation publisher;
+	private AudioTrack[] tracks;
+	private File[] booklets;
+	private File[] othres;
+	private String comment;
 	
+	/*
 	public int getDiskNumber(){
 		return disks == null? 0: disks.length;
 	}
-
+	*/
 }
--- src/com/dayanuyim/ostreammy/entity/AudioTrack.java
+++ src/com/dayanuyim/ostreammy/entity/AudioTrack.java
@@ -1,16 +1,21 @@
 package com.dayanuyim.ostreammy.entity;
 
-import java.security.KeyStore.PrivateKeyEntry;
+import java.io.File;
 
 public class AudioTrack {
-	private String title;
-	private Person artist;
 	private Album album;
-	private String genre;
-	private String comment;
-	private Person composer;
+	private String title;
+	private String no;
+	private int length; //sec
+	private Person[] artists;
 	private Person originalArtist;
+	private Person composer;
+	private String genre;
 	private String url;
+	private String comment;
+	private String sha1;
 	private String encoder;
-	private Person publisher;
+	private int bitrate;
+	private boolean vbr;
+	private File location;
 }
