f82b3e8baa50c49e83fcb96795083f608910df11
TsungtaTsai <tsungtatsai@iii.org.tw>
2016-09-14 15:17:32 +0800

    [Config] Add UTF8 filter to set char encoding as UTF8

--- src/com/dayanuyim/ostreammy/config/WebAppInitializer.java
+++ src/com/dayanuyim/ostreammy/config/WebAppInitializer.java
@@ -1,9 +1,12 @@
 package com.dayanuyim.ostreammy.config;
 
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import static org.apache.commons.lang3.ArrayUtils.*;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
 {
@@ -25,5 +28,10 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 	@Override
 	protected void customizeRegistration(Dynamic registration) {
 		registration.setMultipartConfig(new MultipartConfigElement(""));
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		return toArray(new UTF8Filter());
 	}
 }
